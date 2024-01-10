f6b18dccf98a6859905898977d68bb1e08c24614
Matthew Milam <96691690+08milam@users.noreply.github.com>
2024-01-10 13:13:11 -0600

    overhaul all code

--- script.js
+++ script.js
@@ -1,58 +1,97 @@
-
-
-/** Type of vehicle */
-const form = document.querySelector('#form')
-
-form.addEventListener('submit', function(event){
-
-  make = document.querySelector('#make').value
-  model = document.querySelector('#model').value
-  year = document.querySelector('#year').value
-
-
-  class vehicle{
-
-   constructor(make, model, year){
-       this.make = make
-       this.model = model;
-       this.year = year;
-   }
-   
-   honk(){
-       return 'Beep'
-   }
-   toString(){
-       return `${this.make} ${this.model} ${this.year}`
-   }
-   
+// PORJECT NAME: Car OO Challenge
+// FILE: script.js
+// DATE: 1/9/24
+// MOD DATE:
+// MOD CHANGES:
+// MOD LINE:
+// VERSION: 0.0.1
+// PROGRAMER: Charles Matthew Milam Jr
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+// Constructor to initialize make, model, and year of the vehicle
+class vehicle {
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+  honk() {
+    return "BEEP";
+  }
+  revEngine() {
+    return "VROOM";
+  }
+  toString() {
+    return `Make: ${this.make} Model: ${this.model} Year: ${this.year}`;
+  }
 }
 
-console.log(vehicle.honk())
-// MFV = My First Vehicle
-let MFV = new vehicle (make, model, year);
-vehicle.toString() + vehicle.honk();
+capacity = 2;
 
-  event.preventDefault()
+const listOfVehicle = [];
 
-console.log(MFV)
-})
-
-
-
-add(newVehicle) {
-  if (!(newVehicle instanceof Vehicle)) {
-    return "Only vehicles are allowed in here!";
+function addToGarage() {
+  class Garage {
+    constructor(capacity) {
+      this.listOfVehicle = [];
+      this.capacity = capacity;
+    }
+    add() {
+      // Call the inputvalues function to gather user input for a new vehicle
+      let addVehicle = inputvalues();
+      return addVehicle;
+    }
   }
-  if (this.vehicles.length >= this.capacity) {
-    return "Sorry, we're full.";
-  }
-  this.vehicles.push(newVehicle);
-  return "Vehicle added!";
+
+  const addTo = new Garage();
+
+  let added = addTo.add();
 }
 
+function inputvalues() {
+  const type = document.querySelector("#type").value;
+  const make = document.querySelector("#make").value;
+  const model = document.querySelector("#model").value;
+  const year = document.querySelector("#year").value;
 
+  const enteredVehicle = new vehicle(make, model, year);
+  listOfVehicle.push(enteredVehicle);
+  // Define constants for vehicle types
+  let motorcycle = "motorcycle";
+  let car = "car";
+  let vType = type;
+  let empty = "empty";
+  str = enteredVehicle.toString();
+  Honk = enteredVehicle.honk();
+  Rev = enteredVehicle.revEngine();
+  // Check the vehicle type and display information accordingly
+  if (vType.includes(empty)) {
+    alert("Only vehicles are allowed in here");
+  } else if (vType.includes(motorcycle)) {
+    numWheels = 2;
+    postVehicle().textContent = str;
+    postVehicle().textContent = Rev;
+    postVehicle().textContent = "Number of wheels" + numWheels;
+  } else {
+    numWheels = 4;
+    postVehicle().textContent = str;
+    postVehicle().textContent = Honk;
+    postVehicle().textContent = "Number of wheels" + numWheels;
+  }
 
+  if (listOfVehicle.length > 5) {
+    alert("Sorry, we're full.");
+  }
+}
+// Function to create and append a new paragraph element for displaying vehicle information
 
-// const output = document.querySelector('#output')
-// statment.innerText = 'Your Make is ' + make + ', model ' + model + ' and year is ' + year
-// output.appendChild(statment)
+function postVehicle() {
+  let output = document.querySelector("#output");
+  let div = document.createElement("div");
+  div.className = "vBlock";
+  output.appendChild(div);
+  let p = document.createElement("p");
+  div.appendChild(p);
+  return p;
+}
+
+document.querySelector("#submitBtn").addEventListener("click", addToGarage);
